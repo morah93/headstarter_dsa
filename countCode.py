194055e9f0540925d425350e2380f5d406dae0fe
Mohammed Rahman <97058803+morah93@users.noreply.github.com>
2024-06-22 16:10:08 -0400

    countCode

---
+++ countCode.py
@@ -0,0 +1,8 @@
+# Given an input string, return the number of times that the string "code" appears anywhere in the string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+
+# count_code('aaacodebbb') → 1
+# count_code('codexxcode') → 2
+# count_code('cozexxcope') → 2
+
+def count_code(s):
+  pass
